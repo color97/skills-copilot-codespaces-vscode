bca0d5e33ce66d1fbefaedd4a44c7068dac0893f
wujiale <wujiale0907@qq.com>
2023-08-07 22:00:08 +0800

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberValueLength = skillsMemberValue.length;
+    var skillsMemberValueLengthMax = skillsMember.getAttribute("maxlength");
+    var skillsMemberValueLengthMax = parseInt(skillsMemberValueLengthMax);
+    var skillsMemberValueLengthMax = skillsMemberValueLengthMax + 1;
+    var skillsMemberValueLengthMax = skillsMemberValueLengthMax - 1;
+    var skillsMemberValueLengthMax = skillsMemberValueLengthMax - skillsMemberValueLength;
+    skills.innerHTML = skillsMemberValueLengthMax;
+}
